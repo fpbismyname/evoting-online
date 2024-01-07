e6aa3085d233f9acd74271c4446ff5c8ef03827e
fpbismyname <fajarpebefajar@gmail.com>
2024-01-07 17:48:30 +0700

    Update V.1.0.1

--- client/src/Pages/nonAdminPage.jsx
+++ client/src/Pages/nonAdminPage.jsx
@@ -12,7 +12,7 @@ const nonAdminPage = () => {
       <div className="flex flex-col items-center justify-center h-screen">
         <div className="bg-blue-300 h-auto w-1/2 py-6 flex flex-col justify-center items-center rounded-xl">
           <h1 className="text-white text-3xl mx-10 font-bold flex justify-center">
-            Voters
+            Voter
           </h1>
         </div>
         <div className="bg-blue-300 h-auto w-1/2 rounded-xl flex flex-col py-6 justify-center items-center mt-5">
